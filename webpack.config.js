d185c1516746048d4d02f97bb7240f2cd98fe15e
Albert Gonzalez <albert.gonzalez@atrapalo.com>
2017-04-07 18:06:35 +0200

    webpack configured

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+const extractSass = new ExtractTextPlugin({
+    filename: "[name].css",
+});
+
+module.exports = {
+    entry: './src/js/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'main.js'
+    },
+    module: {
+        rules: [
+            { test: /\.js$/, use: 'babel-loader' },
+            {
+                test: /\.scss$/,
+                use: extractSass.extract({
+                    use: [{
+                        loader: "css-loader"
+                    }, {
+                        loader: "sass-loader"
+                    }]
+                })
+            }
+        ]
+    },
+    plugins: [
+        extractSass
+    ],
+    devServer: {
+        publicPath: '/dist/',
+        compress: true,
+        port: 9000
+    }
+};
