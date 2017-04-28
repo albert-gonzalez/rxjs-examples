4dfe0eba611da6f0f321282e33d32d6bfaf8a1d3
Albert Gonzalez <albert.gonzalez@atrapalo.com>
2017-04-28 09:13:12 +0200

    example1.js fix and test-custom script added

--- src/js/examples/example1.js
+++ src/js/examples/example1.js
@@ -4,7 +4,7 @@ import { fillElementWithRandomColor } from '../utils/functions';
 export function initialize(Observable = ObservableClass, scheduler = undefined) {
     const buttonClicked =
         Observable
-            .fromEvent(document.querySelector('.button1'), 'click')
+            .fromEvent(document.querySelector('.example1 .button1'), 'click')
             .delay(1000, scheduler);
 
     const subscriber = buttonClicked.subscribe(() => {
