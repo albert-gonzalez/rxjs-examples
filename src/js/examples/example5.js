55bed36d118f18ee7d218bea8de83399512d9286
Albert Gonzalez <albert.gonzalez@atrapalo.com>
2017-06-28 20:52:53 +0200

    examples 2 to 6

--- src/js/examples/example5.js
+++ src/js/examples/example5.js
@@ -6,5 +6,34 @@ import {
 } from '../utils/functions';
 
 export function initialize(Observable = ObservableClass) {
+    const customObservable = Observable
+        .create(configureObserver)
+        .map(doubleValue);
 
+    const subscription = customObservable
+        .subscribe(
+            (value) => writeTextInElement(value, '.text_5'),
+            (error) => writeTextInElement(error, '.text_5'),
+            () => writeTextInElement('COMPLETED', '.text_5')
+        );
+
+    return [ customObservable, subscription ];
+}
+
+function configureObserver(observer) {
+    addChangeEventListenerToElement('.input_5', (event) => {
+        const value = getElementValueFromEvent(event);
+
+        if (value === 'END') {
+            observer.complete();
+        } else if (!isNaN(value)) {
+            observer.next(value);
+        } else {
+            observer.error('CRITICAL ERROR: Not a number!');
+        }
+    });
+}
+
+function doubleValue(value) {
+   return value * 2;
 }
--- src/js/examples/example6.js
+++ src/js/examples/example6.js
@@ -10,5 +10,28 @@ import {
 } from '../utils/functions';
 
 export function initialize(Observable = ObservableClass, Subject = SubjectClass, scheduler = undefined) {
+    const fromKeyupEventObservable = configureObservable(Observable, scheduler);
+    const fromButtonClickSubject = new Subject();
 
+    addClickEventListenerToElement('.button_6', () => {
+        fromButtonClickSubject.next(getValueFromElement('.input_6'));
+    });
+
+    fromKeyupEventObservable
+        .subscribe(fromButtonClickSubject);
+
+    const subscription = fromButtonClickSubject
+        .subscribe((value) => {
+            searchSpecieAndWriteListInElement(value, '.text_6');
+        });
+
+    return [ fromKeyupEventObservable, fromButtonClickSubject, subscription ];
+}
+
+function configureObservable(Observable, scheduler) {
+    return Observable
+        .fromEvent(getElement('.input_6'), 'keyup')
+        .debounceTime(500, scheduler)
+        .map(((event) => getElementValueFromEvent(event)))
+        .filter((value) => value.length >= 3);
 }
